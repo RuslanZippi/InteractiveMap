862a7236146ccf23c42dd1dbd3feb9053ada3866
Подкуйко Руслан Андреевич <ruslanzippert@gmail.com>
2023-12-13 11:32:35 +0300

    add redirect to Map

--- src/main/java/main/controller/MainController.java
+++ src/main/java/main/controller/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.logging.Logger;
 
@@ -12,6 +13,10 @@ import java.util.logging.Logger;
 public class MainController {
 
     Logger logger = Logger.getLogger(MainController.class.getName());
+    @GetMapping("/")
+    public RedirectView redirectToMain(){
+        return new RedirectView("/map");
+    }
 
     @RequestMapping("/map")
     public String main(){
